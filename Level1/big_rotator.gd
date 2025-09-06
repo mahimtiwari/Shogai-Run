f9e268dddd49b075f88cc9648f59fa789f88302d
mahimtiwari <mahimt.official@gmail.com>
2025-09-06 15:59:05 +0530

    done some changes in the initial stage of my first level to make it better, along with that ive also added sign based randomized L(angular momentum) direction changer

--- Level1/big_rotator.gd
+++ Level1/big_rotator.gd
@@ -1,11 +1,17 @@
 extends Node3D
 
 @export var rotation_speed: float = 2.0  # radians per second
+
 var rng := RandomNumberGenerator.new()
+
+var rotation_dir_k :int = [1,-1].pick_random()
+
+
 func _ready() -> void:
 	rotation.y = rng.randf_range(0,PI*2)
 
 func _process(delta: float) -> void:
-	rotation.y += rotation_speed * delta
+	
+	rotation.y += rotation_dir_k * rotation_speed * delta
 	
 	rotation.y = fmod(rotation.y, TAU)
