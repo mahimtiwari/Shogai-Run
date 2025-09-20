89863e6062c52004e25168630f55e3e094356f67
mahimtiwari <mahimt.official@gmail.com>
2025-09-20 17:18:12 +0530

    made my all of my obstacles compatiable for the new rigid standard

---
+++ general_obst/Rotating/rope_rigid.gd
@@ -0,0 +1,22 @@
+extends RigidBody3D
+
+@export var rotation_speed: float = 2.0  # radians per second
+@export var throw_strength: float = 100000.0  # tweak for power
+
+var rng := RandomNumberGenerator.new()
+var rotation_dir_k :int = [1,-1].pick_random()
+
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var ang_vel = Vector3.FORWARD * rotation_dir_k * rotation_speed
+	state.angular_velocity = ang_vel
+
+
+func _on_body_entered(body: Node) -> void:
+	if body is RigidBody3D:
+		# Compute direction away from center
+		print("collide")
+		var dir = (body.global_transform.origin - global_transform.origin).normalized()
+			# Apply impulse to throw the character
+		body.sleeping = false
+		body.apply_impulse(Vector3.ZERO, dir * throw_strength)
