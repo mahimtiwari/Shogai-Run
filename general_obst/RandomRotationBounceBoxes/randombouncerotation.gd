d4f30f94d02bcaed5c68ae033e33fb285a32f958
mahimtiwari <mahimt.official@gmail.com>
2025-09-22 10:54:07 +0530

    Added arrows and made a basic structure for the 4th sublevel

--- general_obst/RandomRotationBounceBoxes/randombouncerotation.gd
+++ general_obst/RandomRotationBounceBoxes/randombouncerotation.gd
@@ -2,8 +2,15 @@ extends Node3D
 
 @onready var base_cube_block_s_round: Node3D = $baseCubeBlockSRound
 
-func angle_r():
-	return randf_range(0, 20)
-
+func angle_r(base:float):
+	base = deg_to_rad(base)
+	var ve:int = [1,-1].pick_random()
+	
+	return [
+		Vector3(base, 0, 0),
+		Vector3(0, 0, base),
+		Vector3(0, 0, 0)
+	].pick_random()*ve
+	
 func _ready() -> void:
-	base_cube_block_s_round.rotation = Vector3(angle_r(), angle_r(), angle_r())
+	base_cube_block_s_round.rotation = angle_r(10)
