9371f825173c5706e27ae8eaf3992e26c2f61ca6
mahimtiwari <mahimt.official@gmail.com>
2025-09-28 12:36:46 +0530

    enabled camera and character rotation on the disk along with that done some changes in the 6k disk texture

--- general_obst/Rotating/base_rotators.gd
+++ general_obst/Rotating/base_rotators.gd
@@ -13,11 +13,19 @@ func _physics_process(delta: float) -> void:
 		# v = w x r
 		var r_vect:Vector3 = rotator_player_relative_center-player.global_position
 		#GameLevelManager.r_disc_vect=r_vect
-		var vel_vect: Vector3 = -angular_velocity.cross(r_vect)
+		var vel_vect: Vector3 = -angular_velocity.cross(r_vect)*1.018
+		
 		GameLevelManager.disc_env_velocity = vel_vect
 		
 		player.apply_central_force((player.mass * (vel_vect.length()**2) /r_vect.length())*r_vect.normalized())
+		var angle = rotation_speed * delta
+		var rota = Basis(Vector3.UP, angle)
 
+		if GameLevelManager.move_direction == Vector3.ZERO:
+			player.global_transform.basis = rota * player.global_transform.basis
+		else:
+			player.global_transform.basis = player.global_transform.basis.slerp(GameLevelManager.player_global_transform_basis, 0.1)
+			
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
