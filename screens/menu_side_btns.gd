ed954ed7726bcb4ba225228b04a175d288815e3a
mahimtiwari <mahimt.official@gmail.com>
2025-10-02 07:58:18 +0530

    added an animation to the settings bar alsong withthat fixed a few things in the ui

--- screens/menu_side_btns.gd
+++ screens/menu_side_btns.gd
@@ -3,15 +3,27 @@ extends TextureButton
 @export var button_id:=""
 
 var target_scale = Vector2.ONE
+var tex_normal: Texture
+var tex_selected: Texture
 
 func _ready() -> void:
+	connect("pressed", _on_pressed_btn)
+	tex_normal = load("res://screens/menu_ui/%s.png" % button_id)
+	tex_selected = load("res://screens/menu_ui/%s.png" % (button_id + "_selected"))
 	if button_id == GameLevelManager.menu_option_selected:
 		focus_mode = Control.FOCUS_ALL
 		grab_focus()
 
+
 func _process(delta: float) -> void:
+	if GameLevelManager.menu_option_selected == button_id:
+		if texture_normal != tex_selected:
+			texture_normal = tex_selected
+	else:
+		if texture_normal != tex_normal:
+			texture_normal = tex_normal
+	
 	#if GameLevelManager.menu_option_selected == button_id:
-	pass
 #
 	#if is_hovered():
 		#target_scale = Vector2(1.05, 1.05) # hover scale
---
+++ screens/settings.gd
@@ -0,0 +1,22 @@
+extends NinePatchRect
+
+@export var target_position: Vector2 = Vector2(-152.0, 79.0)
+@export var move_duration: float = 0.25
+
+func _process(delta: float) -> void:
+	if GameLevelManager.menu_option_selected=="settings":
+		move_to_target(target_position)
+	else:
+		move_to_target(Vector2(-500.62, target_position.y))
+
+
+var tween: Tween
+
+func move_to_target(pos:Vector2):
+
+	if tween:
+		tween.kill()
+
+
+	tween = create_tween()
+	tween.tween_property(self, "position", pos, move_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
